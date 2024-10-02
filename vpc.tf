6af5299abf183fbc09cf7055a009055039522b7d
Ravikumar Amireddy <ravi.amireddy@encora.com>
2024-10-02 16:36:42 +0530

    terraform module

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "../terraform-aws-vpc"
+  vpc_cidr = var.vpc_cidr
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  public_subnets_cidrs = var.public_subnets_cidrs
+  private_subnets_cidrs = var.private_subnets_cidrs
+  database_subnets_cidrs = var.database_subnets_cidrs
+  is_peering_required = var.is_peering_required
+}
